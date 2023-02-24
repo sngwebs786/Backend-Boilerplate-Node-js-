c396f71ea93bea66fbe52e3167b6b8fd8da29738
Syeda Nashra Ghaffar <87159019+sngwebs786@users.noreply.github.com>
2023-02-24 17:42:23 +0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+const errorMiddleware = require("./middleware/error");
+app.use(express.json());
+
+//Route Imports
+const product = require("./routes/productRoute");
+
+app.use("/api/v1", product);
+
+//Middleware for Erros
+app.use(errorMiddleware);
+module.exports = app;
